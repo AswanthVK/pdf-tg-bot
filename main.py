1b9b9ba6198ada89ca912914d1e79dc78a8c242e
Andrew <andrew.klyachkin@gmail.com>
2020-12-20 18:44:57 +0200

    close images, minor changes and refactoring

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import os
 import random
 from io import BytesIO
 
-from token import TOKEN
 import requests
 import telebot
 from PIL import Image
 
+from my_token import TOKEN
+
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
-state = dict()
-im_list = dict()
+user_states = dict()
+user_photos = dict()
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -19,22 +20,21 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['new'])
 def send_welcome(message):
-    global state
+    global user_states
     if check_idle(chat_id=message.chat.id):
-        if message.chat.id in im_list:
-            del im_list[message.chat.id][:]
+        if message.chat.id in user_photos:
+            del user_photos[message.chat.id][:]
         else:
-            im_list[message.chat.id] = []
-            state[message.chat.id] = []
+            user_photos[message.chat.id] = []
+            user_states[message.chat.id] = []
         bot.reply_to(message, "Creating a new document. Send me some photos and run /end when you are done.")
-        state[message.chat.id] = "working"
+        user_states[message.chat.id] = "working"
     else:
         bot.reply_to(message, "Already working! Send /end to stop")
 
 
 @bot.message_handler(content_types=['photo'])
 def respond_photo(message):
-    global state
     if check_idle(chat_id=message.chat.id):
         bot.reply_to(message, "You haven't started working yet! Send /new first.")
         return
@@ -42,36 +42,38 @@ def respond_photo(message):
     info = bot.get_file(photo.file_id)
     img_url = "https://api.telegram.org/file/bot{0}/{1}".format(TOKEN, info.file_path)
     response = requests.get(img_url)
-    im_list[message.chat.id].append(Image.open(BytesIO(response.content)))
+    user_photos[message.chat.id].append(Image.open(BytesIO(response.content)))
 
 
 @bot.message_handler(commands=['end'])
 def end(message):
-    global state
+    global user_states
     if check_idle(chat_id=message.chat.id):
         bot.reply_to(message, "You haven't started working yet! Send /new first.")
         return
-    if not im_list:
-        bot.reply_to(message, "You haven't sent any photos, but ok, ending now.")
+    if not user_photos:
+        bot.reply_to(message, "You haven't sent any photos, but ok, ending now. Run /new to make another doc.")
+    bot.send_message(message.chat.id, "Sending a document with {0} photos.".format(len(user_photos[message.chat.id])))
     random.seed()
-    bot.send_message(message.chat.id, "Sending a document with {0} photos.".format(len(im_list[message.chat.id])))
-    docname = "res " + str(message.chat.id) + str(random.randint(a=0, b=1000)) + ".pdf"
-    im_list[message.chat.id][0].save(docname, "PDF", resolution=100.0, save_all=True,
-                                     append_images=im_list[message.chat.id][1:])
+    docname = "res" + str(message.chat.id) + "r" + str(random.randint(a=0, b=1000)) + ".pdf"
+    user_photos[message.chat.id][0].save(docname, "PDF", resolution=100.0, save_all=True,
+                                         append_images=user_photos[message.chat.id][1:])
     send_doc(message.chat.id, docname)
-    bot.send_message(message.chat.id, "Done!")
-    state[message.chat.id] = "idle"
-    del im_list[message.chat.id][:]
+    bot.send_message(message.chat.id, "Done! Run /new to make another doc, or just leave.")
+    user_states[message.chat.id] = "idle"
+    for img in user_photos[message.chat.id]:
+        img.close()
+    del user_photos[message.chat.id][:]
     os.remove(docname)
 
 
 def check_idle(chat_id):
-    return chat_id not in state or state[chat_id] == "idle"
+    return chat_id not in user_states or user_states[chat_id] == "idle"
 
 
 def send_doc(chat_id, doc_path):
-    doc = open(doc_path, 'rb')
-    bot.send_document(chat_id, doc)
+    with open(doc_path, 'rb') as doc:
+        bot.send_document(chat_id, doc)
 
 
 bot.polling()
