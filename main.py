e73ed09c4dd17cf3d3ce6a214a65b1fc7a9f7581
Andrew <andrew.klyachkin@gmail.com>
2021-01-04 23:53:30 +0200

    ASDFAAAAAAAAAAAAAAAAAAAAAaaAAA

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from aiohttp import web
 TOKEN = os.environ["TOKEN"]
 
 WEBHOOK_HOST = 'pdf-tg-bot.herokuapp.com'
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_PORT = int(os.getenv("PORT"))  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
@@ -121,7 +121,7 @@ context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
 # Start aiohttp server
 web.run_app(
     app,
-    host=WEBHOOK_HOST,
+    host=WEBHOOK_LISTEN,
     port=WEBHOOK_PORT,
     ssl_context=context,
 )
